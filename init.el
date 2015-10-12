26c56ece36262b9741f99a1dd497404e2c00cade
February Keeney <february@ihavenotea.com>
2015-10-12 09:35:54 -0700

    Configure ispell

--- init.el
+++ init.el
@@ -119,6 +119,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ispell-program-name "/usr/local/bin/ispell")
  '(web-mode-attr-indent-offset nil)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
